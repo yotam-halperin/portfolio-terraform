6ae001f9209372c474f7f69802184ae606a182a7
yotam-halperin <yotamhalperin7@gmail.com>
2023-02-15 00:27:59 +0200

    kibana works

--- init.sh
+++ init.sh
@@ -13,19 +13,11 @@ echo -e "your ArgoCD user is 'admin'"
 echo -e "your ArgoCD password is '$argoCD_admin_pass'\n\n" 
 
 # Kibana
-kibana_root_pass=$(kubectl get secrets elasticsearch-master-credentials --namespace logging -o yaml | grep -i password | head -1 | tr ':' '\n' | tail -1)
-kibana_root_pass=$(echo $kibana_root_pass | base64 -d)
-echo -e "to connect to Kibana: 'kubectl port-forward -n logging svc/kibana-kibana 8081:5601'\n"
-echo -e "your Kibana user is 'elastic'"
-echo -e "your Kibana password is '$kibana_root_pass'"
+echo -e "to connect to Kibana: 'kubectl port-forward -n logging svc/elasticsearch-kibana 8081:5601'\n"
 
 # echo -e "to connect to Grafana: 'kubectl port-forward -n monitoring svc/argocd-server 8080:80'\n"
 
 # echo -e "to connect to Prometheus: 'kubectl port-forward -n monitoring svc/argocd-server 8080:80'\n"
 
-
-
-
-
 url=$(kubectl get service crossyroad-ingress-nginx-controller | tr -s " " | cut -d" " -f4 | tail -1)
-echo "The URL of the ingress nginx is: '$url'"
+echo -e "The URL of the ingress nginx is: '$url'\n"
--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "yotam-halperin"
+    bucket = "yotam-portfolio"
     key    = "tf-conf/terraform.tfstate"
     region = "eu-west-2"
   }
